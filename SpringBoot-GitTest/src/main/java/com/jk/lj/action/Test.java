c48036bbad5b0566490a663c0f993e9c8ab23059
lj2541924153 <2541924153@qq.com>
2018-01-25 20:19:43 +0800

    第二次提交

---
+++ SpringBoot-GitTest/src/main/java/com/jk/lj/action/Test.java
@@ -0,0 +1,13 @@
+package com.jk.lj.action;
+
+/**
+ * Created by 李劲 on 2018/1/25.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+
+
+}
